8f2fe91661af8a88c20145c751dbd99f14991be0
yuki urkw <urkw.yk@gmail.com>
2019-05-14 02:02:10 +0900

    課題

---
+++ src/components/ItemList.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <div>TodoList</div>
+<div style="float: right">完了済み</div>
+      <p style="text-align: left">未完</p>
+    <div v-for="item in items" :key="item.name">
+      <div class="item">
+        <div v-if="item.price== 1" style="text-align: left">
+          <div class="name">名前: {{ item.name }}</div>
+          <button @click="item.price=-item.price">完了済みに変更</button>
+          <br>
+        </div>
+        <div v-if="item.price== -1" style="text-align: right">
+         <div class="name">名前: {{ item.name }}</div>
+        <button @click="item.price=-item.price">未完に変更</button>
+        <br>
+        </div>
+      </div>
+     </div>
+     <br>
+    <div>
+      <label>
+        名前
+        <input type="text" v-model="newItemName">
+      </label>
+      <button @click="addItem">追加</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TodoList",
+  data() {
+    return {
+      items: [{ name: "example", price: 1 }, { name: "example2", price: 1 }],
+      newItemName: "",
+      newItemPrice: 1
+    };
+  },
+  methods: {
+    addItem() {
+      this.items.push({ name: this.newItemName, price: this.newItemPrice });
+    }
+  }
+  
+};
+</script>
